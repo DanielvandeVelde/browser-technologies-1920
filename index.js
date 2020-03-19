e42a20932a14afea1a2d48b8cb515e9aa43c7704
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-03-19 13:58:34 +0100

    all routes lead to ro- index

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  let queries = {
-    shirtColor: "",
-    shirtNeck: "",
-    shirtText: ""
-  };
-
-  if (Object.entries(req.query).length > 0) {
-    queries = req.query;
-  }
-
-  res.render("index", { queries });
-});
-
 app.get("/creator", (req, res) => {
-  let queries = {
-    shirtColor: "",
-    shirtNeck: "",
-    shirtText: ""
-  };
+  let queries = {};
 
   if (Object.entries(req.query).length > 0) {
     queries = req.query;
   }
 
   res.render("creator", { queries });
+});
+
+app.get("*", (req, res) => {
+  let queries = {};
+
+  if (Object.entries(req.query).length > 0) {
+    queries = req.query;
+  }
+
+  res.render("index", { queries });
 });
 
 app.listen(port, () =>
